135c23c04c866331f93dc6a5b7ec0ba007ea49ce
José Luis Vizcaíno <jlv@myeasyentry.com>
2015-05-10 21:04:54 +0200

    Angular versión bootstrap, se ha puesto jquery como primera dependencia, sigue sin ir la barra lateral

---
+++ assets/js/sidebar.js
@@ -0,0 +1,7 @@
+console.log("test");
+$(document).ready(function () {
+    $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart', function (e) {
+        $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+        e.preventDefault();
+    });
+});
